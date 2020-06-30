24172a63ade695cfe4a594123589266aa6c9e052
Ronit Biswas <ronitbiswas@live.com>
2020-06-30 12:55:04 +0530

    backups made as linux was crashing, switching over to windows

--- movie-catalog-service/src/main/java/com/example/moviecatalog/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalog/resource/MovieCatalogResource.java
@@ -2,13 +2,26 @@ package com.example.moviecatalog.resource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import org.springframework.web.util.UriBuilderFactory;
 
 import com.example.moviecatalog.models.CatalogItem;
 import com.example.moviecatalog.models.Movie;
@@ -20,6 +33,8 @@ public class MovieCatalogResource {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	WebClient.Builder builder = new WebClient.Builder();
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
