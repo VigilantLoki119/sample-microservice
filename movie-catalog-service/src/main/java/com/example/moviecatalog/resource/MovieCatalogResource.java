d10f5ccffc5ac559b86be685cd1966891e6f81b7
Ronit Biswas <ronitbiswas@live.com>
2020-07-08 14:16:24 +0530

    Added load balanced to movie catalog service and changed the url path

--- movie-catalog-service/src/main/java/com/example/moviecatalog/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalog/resource/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.example.moviecatalog.resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +20,17 @@ public class MovieCatalogResource {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-		UserRatings ratings =  restTemplate.getForObject("http://localhost:8082/movies/"+userId, UserRatings.class);
+		UserRatings ratings =  restTemplate.getForObject("http://ratings-data-service/rating/users"+userId, UserRatings.class);
 
 		return ratings.getUserRatings().stream().map(rating -> {
 			
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getRating(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getRating(), Movie.class);
 			
 			return new CatalogItem(movie.getName(), "A movie about bots", rating.getRating());
 			
